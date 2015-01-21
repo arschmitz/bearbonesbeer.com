796520bf228002f22df1d6e37aefa27a11eeb070
Alexander Schmitz <arschmitz@gmail.com>
2015-01-20 22:45:21 -0500

    Update to use google sheets backend and imgur for image hosting

--- js/main.js
+++ js/main.js
@@ -1,73 +1,90 @@
 $(function(){
-	$( "#confirm-age" ).dialog({
-		modal: true,
-		buttons: {
-			"Yes": function(){
-				$( this ).dialog( "close" );
+var beer = {
+	table: function(){
+		$( "table" ).each(function(){
+			$( this ).sheetrock({
+				url: $( this ).attr( "src" ),
+				userCallback: beer.waypoints
+			});
+		});
+	},
+	waypoints: function(){
+		$( ".row-text" ).waypoint({
+			handler: function( dir ) {
+				$( this ).animate({
+					opacity: dir === "down" ? 0 : .8
+				}, 500);
 			},
-			"No": function(){
-				window.location.href = "http://google.com";
+			offset: function(){
+				return ( $( this ).height() * .5 ) * -1;
 			}
-		},
-		width: "auto",
-		classes: {
-			"ui-dialog-titlebar": "hide"
-		}
-	});
-	$( window ).on( "load", function(){
-		console.log( $( ".row-text" ).last().offset().top );
+		});
 		$( ".row-text" ).waypoint({
-			handler: function( direction ) {
-				//console.log( $( this ).text() )
+			handler: function( dir ) {
 				$( this ).animate({
-					opacity: 0.8
-				}, 1000);
+					opacity: dir === "up" ? 0 : .8
+				}, 500);
 			},
-			offset: "55%"
-		});
-		$( ".row-text" ).waypoint({
-			handler: function( direction ) {
-				//console.log( $( this ).text() )
-				$( this ).animate({
-					opacity: 0.8
-				}, 1000);
-			},
-			offset: "20%"
-		});
-		$( ".row-text" ).waypoint({
-			handler: function(){
-				$( this ).css({
-					opacity: 0
-				});
-			},
-			offset: "-300"
-		});
-		$( ".row-text" ).waypoint({
-			handler: function(){
-				$( this ).css({
-					opacity: 0
-				});
-			},
-			offset: "100%"
-		});
-		$( ".row-text" ).last().waypoint({
-			handler: function( direction ) {
-				//console.log( $( this ).text() )
-				$( this ).animate({
-					opacity: 0.8
-				}, 1000);
-			},
-			offset: "80%"
+			offset: function(){
+				return $( window ).height() - ( $( this ).height() / 2 );
+			}
 		});
 		$( ".row-text" ).css({
 			opacity: 0
 		});
+		// Apply active style to menu items as we scroll
+		$( ".scroll-link" ).waypoint(function(){
+			$( "nav .active" ).removeClass( "active" );
+			$( "[href$='" + this.id + "']" ).parent().addClass( "active" );
+		});
+	},
+	navbar: function(){
+		var headers = $( "#main h1" ),
+			html = "";
 
-	});
-	// Apply active style to menu items as we scroll
-  $( ".scroll-link" ).waypoint(function(){
-    $( "nav .active" ).removeClass( "active" );
-    $( "[href$='" + this.id + "']" ).parent().addClass( "active" );
-  });
+		headers.each(function(){
+			var text = $( this ).text();
+			$( this ).append( "<span class='scroll-link' id='" + encodeURI( text ) + "'>&nbsp;</span>" );
+			html += "<li><a href='#" + encodeURI( text ) + "'>" + text + "</a></li>";
+		});
+
+		$( ".nav" ).prepend( html );
+	},
+	fetchRows: function() {
+		var template = Handlebars.compile( $( "#entry-template" ).html() );
+		$( "#main" ).sheetrock({
+			url: "https://docs.google.com/spreadsheets/d/1ExZ37s1BYLBWFX1A5UuAg2MZ9ioKmWTYZ6h8aqGes7w/edit#gid=793522145",
+			rowHandler: template,
+			rowGroups: false,
+			userCallback: function(){
+				beer.table();
+				beer.navbar();
+			}
+		});
+	},
+	confirmAge: function() {
+		$( "#confirm-age" ).dialog({
+			modal: true,
+			buttons: {
+				"Yes": function(){
+					$( this ).dialog( "close" );
+				},
+				"No": function(){
+					window.location.href = "http://google.com";
+				}
+			},
+			width: "auto",
+			classes: {
+				"ui-dialog-titlebar": "hide"
+			}
+		});
+	},
+	brew: function(){
+		beer.confirmAge();
+		beer.fetchRows();
+	}
+};
+
+beer.brew();
 
 });
