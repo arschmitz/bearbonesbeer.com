665001c552cce975e0fbf1525dda29c9f7f87317
Brandon Henry <brandon.m.henry@gmail.com>
2016-09-17 01:51:41 -0400

    use a cookie for age verification

--- staging/js/confirm.js
+++ staging/js/confirm.js
@@ -1,3 +1,39 @@
-function close_box(){
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
+
+function confirm_age(){
   document.getElementById('dialog').style.display = 'none';
-};
+  createCookie("over21", "yes", 7);
+}
+
+function show_box() {
+  document.getElementById('dialog').style.display = 'block';
+}
+
+function check_age() {
+  if (readCookie("over21") != "yes") {
+    show_box();
+  };
+}
